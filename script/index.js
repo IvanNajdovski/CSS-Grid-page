2b8399b42333a5083f78906b901097063ada9c09
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-08-18 16:04:31 +0200

    animation gallery

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 var $window = $(window);
-var $elem = $(".animation")
+var $elem = $(".animation");
+var $move = $(".pic");
 
 function isScrolledIntoView($elem, $window) {
     var docViewTop = $window.scrollTop();
@@ -8,10 +9,27 @@ function isScrolledIntoView($elem, $window) {
     var elemTop = $elem.offset().top;
     var elemBottom = elemTop + $elem.height();
 
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+function isScrolledIntoView($move, $window) {
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+
+    var elemTop = $move.offset().top;
+    var elemBottom = elemTop + $move.height();
+
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 $(document).on("scroll", function () {
     if (isScrolledIntoView($elem, $window)) {
         $elem.addClass("animate")
+    }
+});
+
+$(document).on("scroll", function () {
+    if (isScrolledIntoView($move, $window)) {
+        $move.addClass("moving")
     }
 });
